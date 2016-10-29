12675db63b012ef267aa1f3852e1cd1ec43cda21
Glenn Wiley <glennwiley@gmail.com>
2016-10-29 15:31:07 -0500

    adding bootstrap template, css, js, src and views directories

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 
-var hi = 'hello';
+var app = express();
 
-if (true) {
-    console.log('hi');
-}
-console.log(hi);
+var port = 5000;
+
+app.get('/', function(req, res) {
+    res.send('hello everybody');
+});
+
+app.get('/declan', function(req, res) {
+    res.send('hello declan');
+});
+
+app.listen(port, function(err) {
+    console.log('running server on port ' + port);
+});
